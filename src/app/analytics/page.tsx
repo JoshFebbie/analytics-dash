9ca8f90153c6869a417195f834c5f981726f4827
josh <joshdfebbie@gmial.com>
2024-02-25 21:49:02 -0700

    analytics-tremor

--- src/app/analytics/page.tsx
+++ src/app/analytics/page.tsx
@@ -1,5 +1,16 @@
-const Page = () => {
-  return <p>Hello World</p>
-}
+import { analytics } from "@/utils/analytics"
+import AnalyticsDashboard from "@/components/AnalyticsDashboard"
+
+const Page = async () => {
+  
+  const pageview = await analytics.retrieveDays('pageview', 2)
+
+  return <div className="min-h-screen w-full py-12 flex justify-center
+  items-center">
+    <div className="relative w-full max-w-6xl mx-auto text-white">
+      <AnalyticsDashboard />
+    </div>
+  </div>
+  }
 
 export default Page
